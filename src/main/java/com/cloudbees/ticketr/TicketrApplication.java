a55120ceb02ddb35a3000386b4eeb60258a97395
Muthu Qumar S <muthu.subramanian@cloudsense.com>
2024-04-13 00:06:21 +0530

    Spring init

---
+++ src/main/java/com/cloudbees/ticketr/TicketrApplication.java
@@ -0,0 +1,13 @@
+package com.cloudbees.ticketr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketrApplication.class, args);
+	}
+
+}
